ec8c1845fcf020af29e0fa67332140424334ebb7
Wiliamcerveraa <willicerveraa@gmail.com>
2025-06-20 16:06:49 -0600

    creation of products list application with expo

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Redirect } from 'expo-router';
+import React from 'react';
+
+//En NEXT JS y expo router, los archivos dentro la carpeta app son las pantallas
+
+const App = () => {
+   return <Redirect href="/home" />;
+  // return (
+  //   <SafeAreaView>
+  //     <View className="mt-5 mx-3">
+  //     <Text className="text-3xl" style= {{fontFamily : 'WorkSans-Black'}}>Hola Mundo</Text>
+  //     <Text className="text-3xl font-work-black text-primary">Hola Mundo</Text>
+  //     <Text className="text-3xl font-work-light">Hola Mundo</Text>
+  //     <Text className="text-3xl font-work-medium">Hola Mundo</Text>
+  //   </View>
+  //   </SafeAreaView>
+  // )
+}
+
+export default App;
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,28 @@
+import { FontAwesome } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+
+//tabBarShowLabel : false es para quitar el texto de las tabs, solo mostrar iconos
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heartbeat" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
